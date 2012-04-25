5da25283fa13724ebc6fca554e24e0e327b3a5de
Dane Elwell <dane.elwell@gmail.com>
2012-04-25 05:32:30 +0100

    Script to extract CVE numbers from random text (hopefully).

---
+++ getcves.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/perl
+# Pipe text in, this will pipe out a newline-separated list of CVE numbers present in the file.
+# Maybe.
+
+use strict;
+use warnings;
+
+my @text = <STDIN>;
+my $nextline = 0;
+my $prevline = 0;
+my @cves = ();
+
+foreach (@text) {
+    chomp;
+
+    if ($nextline) {
+        if (m/((^-?\d{4}-\d{4})|(^-?\d{4}))/) {
+            if (!$1) { print "Error parsing: $_\n"; die(1); }
+            my $c = $prevline . $1;
+            push @cves, $c;
+            ($prevline, $nextline) = 0;
+        } else {
+            print "Error parsing:: " . $_;
+            die(2);
+        }
+    }
+
+    while (m/(CVE-\d{4}-\d{4})/g) {
+        push @cves, $1;
+        next;
+    }
+
+    if (m/((CVE-\d{4}-?)|(CVE-?))$/) {
+        $prevline = $1;
+        $nextline = 1;
+    }    
+}
+
+foreach (@cves) {
+    print "$_\n";
+}
