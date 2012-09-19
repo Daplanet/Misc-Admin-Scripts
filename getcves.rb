99b08aa3d8621d090508c312a1cbd6171586bcb6
Dane Elwell <dane.elwell@gmail.com>
2012-09-19 12:30:04 +0100

    Rewrote getcves.pl in Ruby for learning purposes. Needs more error checking.

---
+++ getcves.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby -w
+
+nextline = nil
+prevline = nil
+cves = []
+
+ARGF.lines do |line|
+    line.chomp!
+
+    if nextline
+      matches = /((^-?\d{4}-\d{4})|(^-?\d{4}))/.match(line)
+        cve.push(prevline + matches[0])
+        prevline = nextline = nil
+    end
+
+    /(CVE-\d{4}-\d{4})/.match(line) do |m|
+      cves.push(m[0])
+    end
+
+    /((CVE-\d{4}-?)|(CVE-?))$/.match(line) do |m|
+      prevline = m[0]
+      nextline = 1
+    end
+end
+
+cves.sort.each { |c| puts c }
