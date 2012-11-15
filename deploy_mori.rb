ebd23093636eaeb2bfffdb363030fd067c531bf6
Dane Elwell <dane.elwell@gmail.com>
2012-11-15 15:49:53 +0000

    Removing debug output

--- deploy_mori.rb
+++ deploy_mori.rb
@@ -8,8 +8,6 @@ deploylist = {
   "moritasgus"       =>      "ssh://git@rig.gr:2020/moritasgus.git",
 }
 
-debug = true
-
 deploypath = '/usr/local'
 pidfile = '/tmp/moritasgus.pid'
 moricmdline = 'python /usr/local/moritasgus/moritasgus/moritasgus.py -d -v -m layer4,iptables'
@@ -21,11 +19,9 @@ moricmdline = 'python /usr/local/moritasgus/moritasgus/moritasgus.py -d -v -m la
 deploylist.each do | name, repo |
   fullpath = "#{deploypath}/#{name}"
   if Dir.exists?(fullpath)
-    puts "Already checked out, doing a pull" if debug
     `cd #{fullpath} && git pull > /dev/null 2>&1`
     puts "Error updating repository #{name} from #{repo}" unless $? == 0
   else
-    puts "Cloning repo for the first time" if debug
     `git clone #{repo} #{deploypath}/#{name} > /dev/null 2>&1`
     puts "Error cloning new repository #{name} from #{repo}" unless $? == 0
   end
@@ -35,9 +31,7 @@ pid = 0
 
 if File.exists?(pidfile)
   begin
-    puts "pidfile exists, reading" if debug
     pid = IO.read(pidfile).to_i  
-    puts "pid = #{pid}" if debug
   rescue => err
     puts "Problem reading pidfile: #{err}"
     exit
@@ -45,7 +39,6 @@ if File.exists?(pidfile)
 end
 
 if pid != 0
-  puts "killing process id #{pid}" if debug
   Process.kill("TERM", pid)
   sleep(2)
 end
@@ -53,13 +46,11 @@ end
 begin
   Timeout::timeout(3) do
     begin
-      puts "Ensuring socket is dead..." if debug
       TCPSocket.new('127.0.0.1', 10101).close
       puts "Moritasgus is still running? Not doing anything."
       exit
     rescue Errno::ECONNREFUSED, Errno::EHOSTUNREACH
       # Safe to start, do nothing
-      puts "Nothing running on port 10101, starting Mori" if debug
     end
   end
 rescue Timeout::Error
@@ -67,4 +58,3 @@ rescue Timeout::Error
 end
 
 system(moricmdline)
-puts "Mori started." if debug
